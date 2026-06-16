f71683e1493d573f8b04c31968ad3f8b619361cc
Zohara Duenias <zohara.duenias@gmail.com>
2026-06-16 14:30:52 +0300

    update: add CORS for React development

--- Shva.TransactionApprovalSimulator/Shva.TransactionApprovalSimulator.Api/Program.cs
+++ Shva.TransactionApprovalSimulator/Shva.TransactionApprovalSimulator.Api/Program.cs
@@ -5,10 +5,23 @@ using Shva.TransactionApprovalSimulator.Application.Services;
 using Shva.TransactionApprovalSimulator.Infrastructure.Persistence;
 using Shva.TransactionApprovalSimulator.Infrastructure.Repositories;
 
+const string ReactDevelopmentCorsPolicy = "ReactDevelopmentCorsPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(ReactDevelopmentCorsPolicy, policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173", "http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -31,6 +44,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(ReactDevelopmentCorsPolicy);
 
 app.UseAuthorization();
 
